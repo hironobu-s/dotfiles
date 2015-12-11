9c88e4d0598b078c524334e6aaca50cbac89572e
Hironobu Saitoh <hiro@hironobu.org>
2015-12-12 00:00:30 +0900

    authorized_keysをsymlinkにした

--- link.sh
+++ link.sh
@@ -6,7 +6,7 @@ cd ~
 ln -s ${DOTFILES}/.emacs.d ~
 ln -s ${DOTFILES}/.git ~
 ln -s ${DOTFILES}/.gitconfig ~
-ln -s ${DOTFILES}/.ssh ~
+ln -s ${DOTFILES}/.ssh/authorized_keys ~/.ssh/authorized_keys
 ln -s ${DOTFILES}/.tmux.conf ~
 ln -s ${DOTFILES}/.zshrc ~
 
