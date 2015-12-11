374937d927812c51d1b1f21b5ff1f6d6e9acd0a2
Hironobu Saitoh <hiro@hironobu.org>
2015-12-12 00:44:16 +0900

    oh-my-zshのリンクを追加

--- link.sh
+++ link.sh
@@ -8,5 +8,6 @@ ln -s ${DOTFILES}/.gitconfig ~
 ln -s ${DOTFILES}/.ssh/authorized_keys ~/.ssh/authorized_keys
 ln -s ${DOTFILES}/.tmux.conf ~
 ln -s ${DOTFILES}/.zshrc ~
+ln -s ${DOTFILES}/.oh-my-zsh ~
 
 cd $DOTFILES
