6d0fadd016362c32cfd9e9f49b44daeefcda0004
Hironobu Saitoh <hiro@hironobu.org>
2016-02-09 18:26:04 +0900

    helm-find-fileを明示的に指定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -238,6 +238,8 @@
 (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
 (define-key global-map (kbd "C-c i")   'helm-imenu)
 (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+(define-key global-map (kbd "C-x c")   'helm-find-files)
+(define-key global-map (kbd "C-x C-f")   'helm-find-files)
 
 
 ;; open-junk-file
@@ -491,3 +493,20 @@
 ;; (setq server-use-tcp t)
 (unless (server-running-p)
    (server-start))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#000000" "#FAB1AB" "#D1FA71" "#FFA600" "#7b68ee" "#dc8cc3" "#96D9F1" "#FFFFFF"])
+ '(custom-safe-themes
+   (quote
+    ("62e183df524d390b595c0ad1bf1608163768f7ad4f58238b530d5d901be79f00" default)))
+ '(fci-rule-color "#151515"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
