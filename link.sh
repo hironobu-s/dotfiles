5ebf63166effebdbab1f4a49f5d4f2ddc519c979
Hironobu Saitoh <hiro@hironobu.org>
2018-07-09 10:45:55 +0900

    authorized_keysを追記するようにした

--- link.sh
+++ link.sh
@@ -5,9 +5,8 @@ cd ~
 mkdir .config
 
 ln -s ${DOTFILES}/.gitconfig ~
-ln -s ${DOTFILES}/.ssh/authorized_keys ~/.ssh/authorized_keys
+cat ${DOTFILES}/.ssh/authorized_keys >> ~/.ssh/authorized_keys
 ln -s ${DOTFILES}/.tmux.conf ~
-ln -s ${DOTFILES}/.zshrc ~
 ln -s ${DOTFILES}/fish ~/.config/fish
 
 cd $DOTFILES
